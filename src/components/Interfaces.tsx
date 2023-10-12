061bf3b00022d651e6d17cd0fa155acc413adc57
devfabien <ish001fabi@gmail.com>
2023-10-12 17:27:00 +0200

    Tyoescript note App adding the props interface

--- src/components/Interfaces.tsx
+++ src/components/Interfaces.tsx
@@ -2,3 +2,10 @@ export interface notesInt{
     id:string,
     body:string
 }
+export interface props{
+    notes: notesInt[],
+    currentNote: notesInt,
+    setCurrentNoteId: (id: string) => void,
+    newNote:()=> void
+    deleteNote:(id:string)=>void
+}
