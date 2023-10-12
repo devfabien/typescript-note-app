df0f6d927567881c3d3acf934be63f7c06d62c7a
devfabien <ish001fabi@gmail.com>
2023-10-12 17:28:29 +0200

    Tyoescript note App adding the editor file

---
+++ src/components/Editor.tsx
@@ -0,0 +1,31 @@
+import {useState} from "react"
+import ReactMde from "react-mde"
+import Showdown from "showdown"
+import { notesInt} from "./Interfaces"
+
+export default function Editor({ currentNote, updateNote }:{currentNote:notesInt,updateNote:(text:string)=>void}) {
+    const [selectedTab, setSelectedTab] = useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })  
+
+    return (
+        <section className="overflow-y-auto w-4/5 h-screen">
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </section>
+    )
+}
