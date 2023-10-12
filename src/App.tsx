40c360c8731c5eca00c90255ccadd102ed3e2a6c
devfabien <ish001fabi@gmail.com>
2023-10-12 17:19:08 +0200

    Tyoescript note App adding the function to create a new note

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
+import { useState, useEffect } from "react";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+// import { data } from "./data"
+import Split from "react-split";
+import { nanoid } from "nanoid";
+import { notesInt } from "./components/Interfaces";
 
-function App() {
+export default function App() {
+  const def: notesInt[] = [];
+  const [notes, setNotes] = useState( def);
+  const [currentNoteId, setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ""
+  );
+
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type note's title here",
+    };
+    setNotes((prevNotes: notesInt[]) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  }
+
+
   return (
-    <>
-      <h1>Note app</h1>
-    </>
-  )
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="flex">
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+            deleteNote={deleteNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className="w-full h-screen flex flex-col justify-center items-center bg-slate-200">
+          <h1>You have no notes</h1>
+          <button
+            className="cursor-pointer bg-gray-700 text-white rounded-sm p-4"
+            onClick={createNewNote}
+          >
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
 }
-
-export default App
